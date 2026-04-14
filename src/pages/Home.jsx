f6427b3f88a98eb299f73a2d9aa3e1375ee73e5b
Carlos Jose Camero Armenia <carlosjosearmenia@gmail.com>
2026-04-14 11:12:36 -0400

    Se instala mapbox y se suma al home como comentario la instancia del mapa
    Para poder ver el mapa se debe quitar la instancia de comentario en home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
-import ReportModal from "../features/report/components/reportmodal";
+import ReportModal from "../features/report/components/ReportModal";
 import FloatingButton from "../shared/components/FloatingButton";
 import { useReport } from "../features/report/hooks/useReport";
+import Map from "../features/map/components/mapBox";
 
 export default function Home() {
   const [open, setOpen] = useState(false);
@@ -10,17 +11,21 @@ export default function Home() {
   const { submitReport } = useReport();
 
   return (
-    <div>
-      <FloatingButton onClick={() => setOpen(true)} />
 
-      <ReportModal
-        open={open}
-        onClose={() => setOpen(false)}
-        onSubmit={(data) => {
-          submitReport(data);
-          setOpen(false);
-        }}
-      />
-    </div>
+
+      <div>
+
+        {/*<Map />*/}
+        <FloatingButton onClick={() => setOpen(true)} />
+
+        <ReportModal
+          open={open}
+          onClose={() => setOpen(false)}
+          onSubmit={(data) => {
+            submitReport(data);
+            setOpen(false);
+          }}
+        />
+      </div>
   );
 }
