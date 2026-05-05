b8b820ebf5591679086dfcfc6423239439352736
Carlos Jose Camero Armenia <carlosjosearmenia@gmail.com>
2026-05-05 10:52:32 -0400

    Se actualiza router según unificación

--- src/app/Router.jsx
+++ src/app/Router.jsx
@@ -3,7 +3,6 @@ import Layout from "../app/Layout";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
-import MapaB from "../pages/MapaB";
 
 export default function AppRouter() {
     return (
@@ -13,7 +12,6 @@ export default function AppRouter() {
                     <Route path="/" element={<Home/>} />
                     <Route path="/register" element={<Register/>} />
                     <Route path="/login" element={<Login/>} />
-                    <Route path="/mapa" element={<MapaB/>} />
                 </Route>
             </Routes>
         </BrowserRouter>
