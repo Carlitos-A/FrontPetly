4525887312f2b102c2897c3bcdc0f8bc8d1ab575
Carlos Jose Camero Armenia <carlosjosearmenia@gmail.com>
2026-06-01 11:30:53 -0400

    Se agrega analisis de coverage general
    Se realiza la implementacion de un plugin capaz de calcular el coverage general del codigo en cuanto pruebas unitarias, esto pidiendo analizar el codigo en su mayoría para asi tener un resultado amplio y veridico del coverage del front completo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'  
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ]})
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test/setup.js'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+
+      include: [
+        'src/**/*.js',
+        'src/**/*.ts',
+        'src/**/*.jsx',
+        'src/**/*.tsx',
+      ],
+    },
+  },
+})
